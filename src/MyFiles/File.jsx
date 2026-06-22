692d6bbe0a65a595993252fc4139e0c937a2354d
Cosmin Nichita <nichitacosmin35@gmail.com>
2026-06-22 09:58:25 +0300

    Website is now fully responsive

--- src/MyFiles/File.jsx
+++ src/MyFiles/File.jsx
@@ -9,13 +9,14 @@ import Rename from "../assets/SVG/FileIcons/Rename.svg?react"
 import Trash from "../assets/SVG/FileIcons/Trash.svg?react"
 import StarFull from "../assets/SVG/StarFull.svg?react"
 import Group from "../assets/SVG/UserIcons/Group.svg?react"
-
+import ArrowDown from "../assets/SVG/ArrowDown.svg?react" 
 export default function File(props) {
     const { pathname } = useLocation()
     const displayOwner = (pathname.includes("shared") || pathname.includes("favorites") || pathname.includes("trash"))
     const isFolder = props.extension === "folder"
 
     const [showMenu, setShowMenu] = useState(false)
+    const [isExpanded, setIsExpanded] = useState(false) 
     const menuRef = useRef(null)
 
     useEffect(() => {
@@ -40,8 +41,13 @@ export default function File(props) {
     }
 
     return (
-        <div className="fileRow">
+        <div className={`fileRow ${isExpanded ? "expanded" : ""}`} onClick={() => setIsExpanded(p => !p)}>
             <div className="fileNameCell">
+                {/* Butonul de expandare (vizibil doar pe mobil) */}
+                <div className="mobileExpandBtn" onClick={(e) => { e.stopPropagation(); setIsExpanded(p => !p) }}>
+                    <ArrowDown className={`expandIcon ${isExpanded ? "rotated" : ""}`} />
+                </div>
+
                 <div className="fileIconWrapper">
                     {isFolder ? (
                         <Folder />
@@ -50,13 +56,14 @@ export default function File(props) {
                     )}
                 </div>
 
-                <h3>{props.name}</h3>
+                <h3 className="itemName">{props.name}</h3>
             </div>
             
-            <h3>{isFolder ? "Folder" : props.extension}</h3>
-            <h3>{props.size ? props.size : "-"}</h3>
-            <h3>{props.lastModified}</h3>
-            {displayOwner && <h3>{props.owner}</h3>}
+            {/* Informațiile ascunse pe mobil până la expandare */}
+            <h3 className="fileDetail"><span className="mobileLabel">Type: </span>{isFolder ? "Folder" : props.extension}</h3>
+            <h3 className="fileDetail"><span className="mobileLabel">Size: </span>{props.size ? props.size : "-"}</h3>
+            <h3 className="fileDetail"><span className="mobileLabel">Modified: </span>{props.lastModified}</h3>
+            {displayOwner && <h3 className="fileDetail"><span className="mobileLabel">Owner: </span>{props.owner}</h3>}
 
             <div className="fileOptionsContainer" ref={menuRef}>
                 <Dots 
@@ -69,34 +76,22 @@ export default function File(props) {
 
                 {showMenu && <div className="fileDropdownMenu">
                     <div className="pathMenuOption" onClick={(e) => handleAction(e, "download")}>
-                        <Download />
-                        <h5>Download</h5>
+                        <Download /><h5>Download</h5>
                     </div>
-
                     <hr />
-
                     <div className="pathMenuOption" onClick={(e) => handleAction(e, "rename")}>
-                        <Rename />
-                        <h5>Rename</h5>
+                        <Rename /><h5>Rename</h5>
                     </div>
-
                     <hr />
-
                     <div className="pathMenuOption" onClick={(e) => handleAction(e, "delete")}>
-                        <Trash />
-                        <h5>Delete</h5>
+                        <Trash /><h5>Delete</h5>
                     </div>
-
                     <div className="pathMenuOption" onClick={(e) => handleAction(e, "favorites")}>
-                        <StarFull />
-                        <h5>Favorites</h5>
+                        <StarFull /><h5>Favorites</h5>
                     </div>
-
                     <hr />
-
                     <div className="pathMenuOption" onClick={(e) => handleAction(e, "access")}>
-                        <Group />
-                        <h5>Manage Access</h5>
+                        <Group /><h5>Manage Access</h5>
                     </div>
                 </div>}
             </div>
