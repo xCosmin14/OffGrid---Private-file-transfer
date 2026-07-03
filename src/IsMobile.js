9d74bf5cd613eb192f83806b9ea958487aee201c
AndreeaBaltag <deeaparaschiva@gmail.com>
2026-07-03 14:31:05 +0300

    removed everything

--- src/IsMobile.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-export default function isMobile() {
-    //if (navigator.userAgentData) return navigator.userAgentData.mobile && window.innerWidth <= 500;  
-    //return /Mobi|Android/i.test(navigator.userAgent) && window.innerWidth <= 500;
-
-    //metoda de mai sus o sa fie implementata cand termin designul
-    return window.innerWidth <= 500;
-}
